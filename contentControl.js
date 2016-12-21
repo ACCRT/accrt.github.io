20d5a1913c7d0a63e60f09376d999adbde17997c
张云翔 <994498752@qq.com>
2016-12-21 18:49:24 +0800

    add auto fit of video size

--- contentControl.js
+++ contentControl.js
@@ -1,7 +1,50 @@
 angular.module('App', [])
-.controller('ContentController', function () {
+.controller('ContentController', function ($scope) {
     var content = this;
     content.page = 1;
+
+    $scope.$watch(
+        function () { return document.getElementById('billboard').scrollWidth },
+        function (newValue, oldValue) {
+            console.warn(newValue);
+            if(newValue != 0)
+                document.getElementById('billboard').height = newValue / 560 * 420;
+        });
+
+    $scope.$watch(
+        function () { return document.getElementById('hellocorn').scrollWidth },
+        function (newValue, oldValue) {
+            if (newValue != 0)
+                document.getElementById('hellocorn').height = newValue / 560 * 420;
+        });
+
+    $scope.$watch(
+        function () { return document.getElementById('musicube1').scrollWidth },
+        function (newValue, oldValue) {
+            if (newValue != 0)
+                document.getElementById('musicube1').height = newValue / 560 * 420;
+        });
+
+    $scope.$watch(
+        function () { return document.getElementById('musicube2').scrollWidth },
+        function (newValue, oldValue) {
+            if (newValue != 0)
+                document.getElementById('musicube2').height = newValue / 560 * 420;
+        });
+
+    $scope.$watch(
+        function () { return document.getElementById('memorycage').scrollWidth },
+        function (newValue, oldValue) {
+            if (newValue != 0)
+                document.getElementById('memorycage').height = newValue / 560 * 420;
+        });
+
+    $scope.$watch(
+        function () { return document.getElementById('supperzzle').scrollWidth },
+        function (newValue, oldValue) {
+            if (newValue != 0)
+                document.getElementById('supperzzle').height = newValue / 560 * 420;
+        });
 
     content.changePage = function(page)
     {

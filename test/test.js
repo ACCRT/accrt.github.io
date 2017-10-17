453c1eb4d688b786ff4625d6f1d1e9f4a4ec5167
张云翔 <994498752@qq.com>
2017-10-16 23:05:18 -0500

    test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,11 @@ window.onload = function () {
     document.cookie = "city1=Houston";
     document.cookie = "city2=Dallas";
 
+    var expiry = new Date();
+    expiry.setDate(1);
+    document.cookie = "city2=Austin; expires=" +
+                          expiry.toString();
+
     if (document.cookie.split(";").length > 1) {
         document.cookie = "state=TX";
     }
